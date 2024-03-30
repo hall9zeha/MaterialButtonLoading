8555100871105ef47220e8754040c4b611ad892b
hall9zeha <vmwaretars@gmail.com>
2024-03-30 10:26:29 -0500

    Created material button loading librarie

---
+++ MaterialButtonLoading/src/main/java/com/barryzeha/materialbuttonloading/components/ButtonLoading.kt
@@ -0,0 +1,11 @@
+package com.barryzeha.materialbuttonloading.components
+
+
+/**
+ * Project MaterialButtonLoading
+ * Created by Barry Zea H. on 30/3/24.
+ * Copyright (c)  All rights reserved.
+ **/
+
+class ButtonLoading {
+}
