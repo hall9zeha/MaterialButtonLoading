ead89ecfc0652bd2256a18ae46022d33c69b46c7
hall9zeha <vmwaretars@gmail.com>
2024-04-03 15:35:54 -0500

    Change  custom view

--- MaterialButtonLoading/src/main/java/com/barryzeha/materialbuttonloading/components/ButtonLoading.kt
+++ MaterialButtonLoading/src/main/java/com/barryzeha/materialbuttonloading/components/ButtonLoading.kt
@@ -8,16 +8,14 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.RectF
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.TypedValue
+import android.view.Gravity.CENTER
 import android.widget.RelativeLayout
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import com.barryzeha.materialbuttonloading.R
-import com.google.android.material.progressindicator.CircularProgressIndicator
 import kotlin.math.min
 
 
@@ -33,26 +31,34 @@ class ButtonLoading @JvmOverloads constructor(
     defStyleAttr:Int=0,
     ):RelativeLayout(context,attrs,defStyleAttr) {
 
-    private val cornerRadius = 20f
+    private var cornerRadius:Float? = null
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val path = Path()
     private val rect = RectF()
-    private val contentMain:RelativeLayout
-    private var progressBar: CircularProgressIndicator
-    private val buttonTextView: TextView
-    private var textColor = androidx.appcompat.R.attr.titleTextColor
+    private var colorStroke:Int? = null
 
+    private val textView: TextView
 
     init {
-        paint.style = Paint.Style.FILL
-        val array: TypedArray = context.obtainStyledAttributes(intArrayOf(android.R.attr.colorPrimary))
-        val defaultTextColor = array.getColor(0, 0)
-        paint.color = resources.getColor(android.R.color.holo_blue_light)
+        setBackgroundColor(Color.TRANSPARENT)
+        val params = LayoutParams(
+            LayoutParams.WRAP_CONTENT,
+            LayoutParams.WRAP_CONTENT
+        )
 
-        val root = LayoutInflater.from(context).inflate(R.layout.button_loading_layout, this, true)
-        contentMain = root.findViewById(R.id.contentMain)
-        buttonTextView = root.findViewById(R.id.tvButton)
-        progressBar = root.findViewById(R.id.pbLoading)
+        textView = TextView(context)
+        textView.setPadding(22,22,22,22)
+        // Configurar el texto y otros atributos del TextView según sea necesario
+        textView.text = "Texto de ejemplo"
+        textView.setTextColor(Color.WHITE)
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14f)
+        textView.setTypeface(textView.typeface, Typeface.BOLD)
+        textView.gravity= CENTER
+        params.addRule(CENTER_IN_PARENT)
+        textView.layoutParams=params
+
+        // Añadir el TextView como vista hijo
+        addView(textView)
         loadAttr(attrs, defStyleAttr)
     }
 
@@ -65,17 +71,22 @@ class ButtonLoading @JvmOverloads constructor(
             0
         )
 
+        val array: TypedArray = context.obtainStyledAttributes(intArrayOf(android.R.attr.textColorPrimaryInverse,android.R.attr.colorPrimary))
+        val defaultTextColor = array.getColor(0, 0)
+        val defaultColor = array.getColor(1, 0)
+
         val buttonText = arr.getString(R.styleable.loadingButtonStyleable_text)
+        cornerRadius = arr.getDimension(R.styleable.loadingButtonStyleable_cornerRadius, 50F)
         val loading = arr.getBoolean(R.styleable.loadingButtonStyleable_loading, false)
         val enabled = arr.getBoolean(R.styleable.loadingButtonStyleable_enabled, true)
         val colorText = arr.getString(R.styleable.loadingButtonStyleable_textColor)
-        val array: TypedArray = context.obtainStyledAttributes(intArrayOf(android.R.attr.textColorPrimaryInverse))
-        val defaultTextColor = array.getColor(0, 0)
-        textColor = if(!colorText.isNullOrEmpty()) Color.parseColor(colorText) else defaultTextColor
+        val strokeColor = arr.getString(R.styleable.loadingButtonStyleable_colorStroke)
+
+        val textColor = if(!colorText.isNullOrEmpty()) Color.parseColor(colorText) else defaultTextColor
+        colorStroke = if(!strokeColor.isNullOrEmpty()) Color.parseColor(strokeColor) else defaultColor
 
         arr.recycle()
         isEnabled = enabled
-        buttonTextView.isEnabled = enabled
         setText(buttonText)
         //progressBar.setAnimation(lottieResId)
         setLoading(loading)
@@ -86,40 +97,60 @@ class ButtonLoading @JvmOverloads constructor(
     fun setLoading(loading: Boolean){
         isClickable = !loading //Disable clickable when loading
         if(loading){
-            buttonTextView.visibility = View.GONE
-            progressBar.visibility = View.VISIBLE
+            /*buttonTextView.visibility = View.GONE
+            progressBar.visibility = View.VISIBLE*/
 
         } else {
-            buttonTextView.visibility = View.VISIBLE
-            progressBar.visibility = View.GONE
+           /* buttonTextView.visibility = View.VISIBLE
+            progressBar.visibility = View.GONE*/
         }
     }
     fun setTextColor(color:Int){
-        buttonTextView.setTextColor(color)
+
     }
 
     fun setLoadingColor(color:Int){
-        progressBar.trackColor = color
+
 
     }
     fun setText(text : String?) {
-        buttonTextView.text = text
+
     }
 
     override fun setEnabled(enabled: Boolean) {
         super.setEnabled(enabled)
-        buttonTextView.isEnabled = enabled
+
     }
 
     override fun onDraw(canvas: Canvas) {
         val width = width.toFloat()
         val height = height.toFloat()
+        val corners=
+        cornerRadius?.let{
+            24F
+        }?.run{
+            cornerRadius
+        }
 
-        // Dibuja el fondo redondeado
+        val array: TypedArray = context.obtainStyledAttributes(intArrayOf(android.R.attr.colorPrimary))
+        val defaultColor = array.getColor(0, 0)
+
+        // Dibujar el fondo redondeado
+        paint.style = Paint.Style.FILL
+        paint.color = defaultColor
         rect.set(0f, 0f, width, height)
         path.reset()
-        path.addRoundRect(rect, cornerRadius, cornerRadius, Path.Direction.CW)
+        path.addRoundRect(rect, corners!!, corners, Path.Direction.CW)
         canvas.drawPath(path, paint)
+
+        // Dibujar el borde redondeado
+        paint.style = Paint.Style.STROKE
+        val cStroke=colorStroke?.let{defaultColor}?.run{colorStroke}
+        paint.color = cStroke!!
+        paint.strokeWidth = 3f // Establecer el ancho del trazo si es necesario
+        canvas.drawRoundRect(rect, corners, corners, paint)
+
+
     }
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
@@ -146,7 +177,9 @@ class ButtonLoading @JvmOverloads constructor(
             MeasureSpec.UNSPECIFIED->defaultButtonHeight
             else->defaultButtonHeight
         }
-        measureChild(contentMain,width, height)
+
         setMeasuredDimension(width, height)
     }
+
 }
+
